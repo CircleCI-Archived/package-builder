7100df223d7336898a7cc8fbde271f1d11d1f53f
Kim, Hirokuni <yangkookkim@gmail.com>
2015-12-19 21:53:54 +0900

    wip

---
+++ python/python/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+set -x
+
+version=$1
+
+pushd src/
+docker build --build-arg version=${version} -t kimh/python-build:${version} .
+docker run kimh/python-build:${version} cat python_${version}_amd64.deb > python_${version}_amd64.deb
+popd
+
+pushd test/
+cp ../src/python_${version}_amd64.deb .
+docker build --build-arg version=${version} .
+popd 
